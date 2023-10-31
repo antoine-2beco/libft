d49096be541a4c8d57a79f98aeb37bb13e78eeff
antoine-2beco <mail@antoinedebeco.com>
2023-10-31 10:59:11 +0100

    3.1

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 12:35:06 by ade-beco          #+#    #+#             */
-/*   Updated: 2023/10/26 14:40:20 by ade-beco         ###   ########.fr       */
+/*   Updated: 2023/10/27 14:09:54 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	else if (len < ft_strlen(s) - start)
 		str = (char *) malloc(len + 1);
 	else
-		str = (char *)malloc((ft_strlen(s) - start) + 1);
+		str = (char *) malloc((ft_strlen(s) - start) + 1);
 	if (!str)
 		return (NULL);
 	while (s[i])
