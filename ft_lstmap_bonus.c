ab4f6827f1239904f693f41ea0b4d1ccdf9dc9fa
antoine-2beco <mail@antoinedebeco.com>
2023-11-07 18:53:10 +0100

    5.2

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 14:53:57 by ade-beco          #+#    #+#             */
-/*   Updated: 2023/11/07 18:30:40 by ade-beco         ###   ########.fr       */
+/*   Updated: 2023/11/07 18:46:36 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 			while (out)
 			{
 				new = out->next;
-				(*del)(out->content);
+				del(out->content);
 				free(out);
 				out = new;
 			}
